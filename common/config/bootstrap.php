d1164196c0c5a5d36e718640e5237be81c0bd6e1
Aleksandr Rikun <rtvs.net@gmail.com>
2019-05-21 12:34:05 +0300

    First Commit

---
+++ common/config/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+Yii::setAlias('@common', dirname(__DIR__));
+Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
+Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
+Yii::setAlias('@api', dirname(dirname(__DIR__)) . '/api');
+Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
+Yii::setAlias('@componets', dirname(dirname(__DIR__)) . '/components');
+Yii::setAlias('@vendor', dirname(dirname(__DIR__)) . '/vendor');
+Yii::setAlias('@bower', dirname(dirname(__DIR__)) . '/vendor/bower');
+Yii::setAlias('@sb-admin', dirname(dirname(__DIR__)) . '/vendor/bower/bootstrap-sb-admin');
+
